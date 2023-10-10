f06973e57a0cc012a21532e3ce2e2cb4d7212def
seanmckee <seanmingmckee@gmail.com>
2023-10-09 18:21:45 -0700

    set up, auth, onboard, backend

---
+++ lib/actions/thread.actions.ts
@@ -0,0 +1,8 @@
+interface Params {
+  text: string;
+  author: string;
+  communityId: string | null;
+  path: string;
+}
+
+export async function createThread({ text, author, communityId, path }: Params);
