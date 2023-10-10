639c7b5b0ea9ac47e6c6bc0b81a5b8b8af2bba78
seanmckee <seanmingmckee@gmail.com>
2023-10-10 00:45:27 -0700

    added thread card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,35 @@
+import ThreadCard from "@/components/cards/ThreadCard";
 import { fetchPosts } from "@/lib/actions/thread.actions";
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, currentUser } from "@clerk/nextjs";
 
 export default async function Home() {
   const result = await fetchPosts(1, 30);
+  const user = await currentUser();
 
-  console.log(result);
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
+      <section className="mt-9 flex felx-col gap-10">
+        {result.posts.length === 0 ? (
+          <p>No Threads Found</p>
+        ) : (
+          <>
+            {result.posts.map((post) => (
+              <ThreadCard
+                key={post.id}
+                id={post._id}
+                currentUserId={user?.id || ""}
+                parent={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
     </>
   );
 }
---
+++ components/cards/ThreadCard.tsx
@@ -0,0 +1,42 @@
+interface Props {
+  id: string;
+  currentUserId: string;
+  parent: string | null;
+  content: string;
+  author: {
+    name: string;
+    image: string;
+    id: string;
+  };
+  community: {
+    name: string;
+    image: string;
+    id: string;
+  } | null;
+  createdAt: string;
+  comments: {
+    author: {
+      image: string;
+    };
+  }[];
+  isComment?: boolean;
+}
+
+const ThreadCard = ({
+  id,
+  currentUserId,
+  parent,
+  content,
+  author,
+  community,
+  createdAt,
+  comments,
+}: Props) => {
+  return (
+    <article>
+      <h2 className="text-small-regular text-light-2">{content}</h2>
+    </article>
+  );
+};
+
+export default ThreadCard;
