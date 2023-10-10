033501a38d94258d35f594980bb80e3e0da26255
seanmckee <seanmingmckee@gmail.com>
2023-10-10 01:48:56 -0700

    thread card styling

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
-      <section className="mt-9 flex felx-col gap-10">
+      <section className="mt-9 flex flex-col gap-10">
         {result.posts.length === 0 ? (
           <p>No Threads Found</p>
         ) : (
