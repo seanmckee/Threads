d0d731e53c4ec7a88df0fcac44009356298694ae
seanmckee <seanmingmckee@gmail.com>
2023-11-08 15:17:51 -0800

    fetch user posts action

---
+++ components/shared/ThreadsTab.tsx
@@ -0,0 +1,7 @@
+import exp from "constants";
+
+const ThreadsTab = async () => {
+  return <section>ThreadsTab</section>;
+};
+
+export default ThreadsTab;
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,6 +3,7 @@
 import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
+import Thread from "../models/thread.model";
 
 interface Params {
   userId: string;
@@ -47,4 +48,29 @@ export async function fetchUser(userId: string) {
     //   model: Community,
     // });
   } catch (error) {}
+}
+
+export async function fetchUserPosts(userId: string) {
+  try {
+    connectToDB();
+
+    // Find all threads authored by user with given user ID
+
+    // Todo: populate with community name
+    const threads = await User.findOne({ id: userId }).populate({
+      path: "threads",
+      model: Thread,
+      populate: {
+        path: "children",
+        model: Thread,
+        populate: {
+          path: "author",
+          model: User,
+          select: "name image id",
+        },
+      },
+    });
+  } catch (error: any) {
+    throw new Error(`Failed to fetch user posts: ${error.message}`);
+  }
 }
